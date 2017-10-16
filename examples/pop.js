c44fae605c015e765084d28745918d73c9bc1824
Stephen Cataldo <github@spaceshare.com>
2017-10-16 15:50:44 -0700

    working on pop and copy-paste

--- examples/pop.js
+++ examples/pop.js
@@ -3,19 +3,35 @@
 
 $(function() {
   
+  /*
   $( ".see3" ).dialog({
     autoOpen: false
   });
-
+  */
 
   /* make it pop: onClick in see1 block, open dialogue for see2 */
   $(".see1").on( "click", function() {
-    console.log( $( this ).text() );
+    console.log( $( this ).children("h2").text() );
     //$(".see3").css("display","block");
     //$( this ).children(".see3").css('display','block');
-    console.log( $( this ).children(".see3").text() );
-		$(".see3").css("display: none;");	
-		$( this ).children(".see3").css("display: block;");
+    //console.log( $( this ).find("div.see3").text() );
+    // reset 
+		$(".see3").css("display", "none");	
+    $(".see3x").css("display", "block"); 
+    // where clicked, instead of hover
+    /*
+    $(".start").scrollTop($(this).scrollTop());
+    console.log("Jump. " + $(this).children("h2").scrollTop() );
+    */
+    // Puttering with window scrolling, seems ok, rebuild with design clearer
+    // Hey - it works, not going to fiddle, but scrolls into view before
+    // making visible
+    $(this).get(0).scrollIntoView();
+    $(this).find("div.see3:last").get(0).scrollIntoView();
+    //$(this).scrollTop(0);
+    $( this ).children(".see3x").css("display", "none"); 
+    $( this ).children("h2").css("display", "block");
+		$( this ).find("div.see3").css("display", "block");
     //$( this ).children(".see3").dialog("open");
   });
 
